2ca4a745d11f73d3399f5f8f7bad83753a05a1f0
bennybop <bennybop74@yahoo.com>
2018-01-16 15:07:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 def fizzbuzz(n)
-  if n % 3 == 0
-     "Fizz"
+  if n % 3 && n % 5 == 0
+     "FizzBuzz"
+  elsif n % 3 == 0
+    "Fizz"
   elsif n % 5 == 0
     "Buzz"
-  elsif n % 3 == 0 && n % 5 == 0
-    "FizzBuzz"
   else
     nil
   end
 end
+
+
+
